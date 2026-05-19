7ecab46511d9e8a50a30d8b662900a3a00e46205
LAM1107 <849404104@qq.com>
2026-05-19 16:04:29 +0800

    支付接口测试

--- locust_shape.py
+++ locust_shape.py
@@ -8,15 +8,21 @@ class SeckillRampUpShape(LoadTestShape):
     6-10 min: 500 users
     """
 
+    # stages = [
+    #     # 阶段 1：0-300 秒，100 用户
+    #     {"duration": 300, "users": 100, "spawn_rate": 10},
+        
+    #     # 阶段 2：300-600 秒，300 用户
+    #     {"duration": 600, "users": 300, "spawn_rate": 10},
+        
+    #     # 阶段 3：600-900 秒，500 用户
+    #     {"duration": 900, "users": 500, "spawn_rate": 10},
+    # ]
+
+
     stages = [
-        # 阶段 1：0-300 秒，100 用户
-        {"duration": 300, "users": 100, "spawn_rate": 10},
-        
-        # 阶段 2：300-600 秒，300 用户
-        {"duration": 600, "users": 300, "spawn_rate": 10},
-        
-        # 阶段 3：600-900 秒，500 用户
-        {"duration": 900, "users": 500, "spawn_rate": 10},
+        # 阶段 1：0-600 秒，100 用户
+        {"duration": 600, "users": 100, "spawn_rate": 10}
     ]
 
     def tick(self):

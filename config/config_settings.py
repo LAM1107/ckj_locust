9a6077445cb6e392a0d7402a08a329fc4ae5689c
LAM1107 <849404104@qq.com>
2026-05-28 16:09:17 +0800

    edit interface

--- config/config_settings.py
+++ config/config_settings.py
@@ -45,7 +45,7 @@ class LoadTestConfig:
     # flow_pay_result_lite / mixed / single_list_detail / single_coupon
     # single_brand / single_reward_brand / single_brand_goods / single_user_goods_one
 
-    SCENARIO_MODE = os.getenv("LOCUST_SCENARIO_MODE", "single_v3_check_goods").lower()
+    SCENARIO_MODE = os.getenv("LOCUST_SCENARIO_MODE", "single_v3_one_cards").lower()
 
     # 轻量压测默认关闭 Prometheus；如果需要 /metrics，再设置 LOCUST_ENABLE_PROMETHEUS=true。
     ENABLE_PROMETHEUS = _env_bool("LOCUST_ENABLE_PROMETHEUS", False)
